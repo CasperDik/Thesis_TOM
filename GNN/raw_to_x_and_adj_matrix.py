9e410d402884e9237248271ad0f59abfd4106b84
CasperDik <casper.dik@home.nl>
2022-09-23 20:32:12 +0200

    commit

--- GNN/raw_to_x_and_adj_matrix.py
+++ GNN/raw_to_x_and_adj_matrix.py
@@ -13,16 +13,24 @@ import networkx as nx
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+import os
 
 
-def delete_hashtag(f):
-    with open(f, "rb") as input_file:
-        s = input_file.read()
-        input_file.close()
-        s = s.replace(b"#", b"")
+def getfiles(path):
+    folder_names = []
+    for entry_name in os.listdir(path):
+        entry_path = os.path.join(path, entry_name)
+        if os.path.isdir(entry_path):
+            folder_names.append(entry_name)
 
-    with open(f, "wb") as output_file:
-        output_file.write(s)
+    filepaths = []
+    for folder in folder_names:
+        if folder[-5:] == "100cm" and folder[:11] == "simulation1":
+            for file in os.scandir(path+ "/" + folder):
+                if file.is_file() and file.path[-4:] == ".txt":
+                    filepaths.append(file.path)
+
+    return filepaths
 
 
 def load_features(f):
@@ -41,43 +49,63 @@ def load_features(f):
     return d
 
 
+def delete_hashtag(f):
+    with open(f, "rb") as input_file:
+        s = input_file.read()
+        input_file.close()
+        s = s.replace(b"#", b"")
+
+    with open(f, "wb") as output_file:
+        output_file.write(s)
+
+
 def feature_matrix(files: list):
     f0 = load_features(files[0])
     f1 = load_features(files[1])
     features = np.stack((f0, f1))
 
     for f in files[2:]:
+        print(f)
         d = load_features(f)
         features = np.vstack((features, d[None, :, :]))
     print(features.shape)
 
     return features
 
-def adj_matrix():
+
+def adj_matrix(N_Nodes: int):
     # get the adjacency matrix as defined in the thesis
     # in GENERAL connect a node n to itself, next node below, node to the right, node to the right above and below diagonally
     # but some exception! --> indicated in the comments below
 
-    N_Nodes = 25
+    # only works if adjacency matrix is a square matrix of NxN
+
     dim = int(np.sqrt(N_Nodes))
     adj_mat = np.zeros((N_Nodes, N_Nodes))
     for n in range(N_Nodes):
         adj_mat[n, n] = 1
 
+        # right
         if not (n > (N_Nodes-dim-1)):     # if at last column, no connection to the right
             adj_mat[n, n+dim] = 1
 
-        if not (n % dim == 4):     # if at bottom row, no connection to next node below or to diagonal below right
+        # below
+        if not (n % dim == (dim-1)):     # if at bottom row, no connection to next node below
             adj_mat[n, n + 1] = 1
-            if not (n > (N_Nodes-dim-1)):      # if not at the bottom, but at the last column, no connection to the right
-                adj_mat[n, n+dim+1] = 1
-                if not (n == 0) | (n % dim == 0):       # if at top row no diagonal connection upper right
-                    adj_mat[n, n+dim-1] = 1
+
+        # diagonal right below
+        if not (n > (N_Nodes-dim-1)) | (n % dim == (dim-1)):      # if at the last column or at the bottom row, no connection to the diagonal right below
+            adj_mat[n, n+dim+1] = 1
+
+        # diagonal right up
+        if not (n == 0) | (n % dim == 0) | (n > (N_Nodes-dim-1)):       # if at top row or at the last column, no diagonal connection upper right
+            adj_mat[n, n+dim-1] = 1
 
     # as the edges are undirected mirror the upper triangle to the lower triangle
     adj_mat = np.triu(adj_mat) + np.tril(adj_mat.T, -1)
 
     return adj_mat
+
 
 def plot_adj_matrix(A):
     # confirms that adjacency matrix is correctly made
@@ -86,13 +114,21 @@ def plot_adj_matrix(A):
     nx.draw_networkx(Graph)
     plt.show()
 
+
 if __name__ == "__main__":
-    files = ["Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m01s.txt", "Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m02s.txt", "Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m03s.txt", "Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m04s.txt"]
-    # F = feature_matrix(files)
-    # todo: save to .npy
-    # todo: get files list --> filter on 100/50cm and different settings?
+    path = "C:/Users/caspe/OneDrive/Documenten/MSc TOM/Thesis TOM/GNN/Data/logs22-04-22"
+    files = getfiles(path)
 
-    A = adj_matrix()
-    plot_adj_matrix(A)
+    # files = ["Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m01s.txt", "Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m02s.txt", "Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m03s.txt", "Data/logs22-04-22/simulation1-50p-50cm/heatmap_08H00m04s.txt"]
 
+    F = feature_matrix(files)
+    N_nodes = len(F[0])
+    A = adj_matrix(N_nodes)
+    # plot_adj_matrix(A)
+
+    print(F.shape)
+    print(A.shape)
+
+    np.save("FeatureMatrix.npy", F)
+    np.save("Adj_Matrix.npy", A)
 
